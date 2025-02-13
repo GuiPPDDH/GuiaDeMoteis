3dc68fdee7177785c26d88b311ee577dbf4d66d7
GuiPPDDH <guipotterrp@gmail.com>
2025-02-12 22:32:46 -0300

    feat: Criado client de conexão RequestHttpClient

---
+++ lib/client/request_http_client.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RequestHttpClient {
+  final String baseUrl;
+
+  RequestHttpClient({required this.baseUrl});
+
+  Future<Map<String, dynamic>> get(String endpoint) async {
+    final url = Uri.parse('$baseUrl/$endpoint');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      try {
+        return jsonDecode(response.body) as Map<String, dynamic>;
+      } catch (e) {
+        throw Exception(
+            'Erro ao decodificar JSON: $e\nResposta: ${response.body}');
+      }
+    } else {
+      throw Exception(
+          'Erro ao buscar os dados: ${response.statusCode}\nResposta: ${response.body}');
+    }
+  }
+}
