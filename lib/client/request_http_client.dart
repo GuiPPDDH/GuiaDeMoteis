7e9c2ee1dbe8abff1d49a38c9a589557df90a316
GuiPPDDH <guipotterrp@gmail.com>
2025-02-27 17:57:29 -0300

    refactor: refatorado tratamento de If-else para switch pattern matching

--- lib/client/request_http_client.dart
+++ lib/client/request_http_client.dart
@@ -14,33 +14,38 @@ class RequestHttpClient {
 
   RequestHttpClient({required this.baseUrl});
 
-  Future<Map<String, dynamic>> get(String endpoint) async {
+  Future<Map<String, dynamic>> get(
+    String endpoint, {
+    Map<String, String>? headers,
+    Map<String, String>? queryParameters,
+  }) async {
     final url = Uri.parse('$baseUrl/$endpoint');
 
     try {
-      final response = await http.get(url).timeout(const Duration(seconds: 10));
+      final response = await http
+          .get(url, headers: headers)
+          .timeout(const Duration(seconds: 10));
 
-      if (response.statusCode == 200) {
-        try {
-          final decodedBody = utf8.decode(response.bodyBytes);
-          return jsonDecode(decodedBody) as Map<String, dynamic>;
-        } on FormatException catch (e) {
-          throw JsonDecodingException(
-              'Erro ao decodificar JSON: ${e.message}\nResposta: ${response.body}');
-        } catch (e) {
-          throw RequestException(
-              'Erro inesperado ao decodificar JSON: $e\nResposta: ${response.body}');
-        }
-      } else if (response.statusCode >= 400 && response.statusCode < 500) {
-        throw ClientErrorException(
-            'Erro do cliente (status code: ${response.statusCode}):\nResposta: ${response.body}');
-      } else if (response.statusCode >= 500) {
-        throw ServerErrorException(
-            'Erro do servidor (status code: ${response.statusCode}):\nResposta: ${response.body}');
-      } else {
-        throw RequestException(
-            'Erro ao buscar os dados (status code: ${response.statusCode}):\nResposta: ${response.body}');
-      }
+      return switch (response.statusCode) {
+        200 => () {
+            try {
+              final decodedBody = utf8.decode(response.bodyBytes);
+              return jsonDecode(decodedBody) as Map<String, dynamic>;
+            } on FormatException catch (e) {
+              throw JsonDecodingException(
+                  'Erro ao decodificar JSON: ${e.message}\nResposta: ${response.body}');
+            } catch (e) {
+              throw RequestException(
+                  'Erro inesperado ao decodificar JSON: $e\nResposta: ${response.body}');
+            }
+          }(),
+        >= 400 && <= 499 => throw ClientErrorException(
+            'Erro do cliente (status code: ${response.statusCode}):\nResposta: ${response.body}'),
+        >= 500 => throw ServerErrorException(
+            'Erro do servidor (status code: ${response.statusCode}):\nResposta: ${response.body}'),
+        _ => throw RequestException(
+            'Erro ao buscar os dados (status code: ${response.statusCode}):\nResposta: ${response.body}'),
+      };
     } on SocketException catch (e) {
       throw NetworkException('Erro de conexão: ${e.message}');
     } on TimeoutException catch (e) {
